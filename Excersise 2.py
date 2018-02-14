7aa822cf718a3e6c386d4fc59f3140dfa1c4b19c
00jnixon <34488801+00jnixon@users.noreply.github.com>
2018-02-14 14:06:56 -0700

    Add files via upload

---
+++ Excersise 2.py
@@ -0,0 +1,9 @@
+# Jason Nixon  Lesson -2 Excersise-2  2-8-2018
+#Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
+
+#Enter your name: Chuck
+#Hello Chuck
+
+print (input("Enter your name: "))
+print ("Hebllo" +"(Chuck)")
+
